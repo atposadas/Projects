63ec8e137959087ae615656b61bafaf6b950019a
atposadas54@gmail.com <atposadas@Heisenberg>
2025-01-31 16:25:38 -0600

    setting smtp tutorial for sending emails

--- tutorial/railsexamples-sending-email/Test/config/environments/development.rb
+++ tutorial/railsexamples-sending-email/Test/config/environments/development.rb
@@ -31,8 +31,20 @@ Rails.application.configure do
   # Store uploaded files on the local file system (see config/storage.yml for options).
   config.active_storage.service = :local
 
+  config.action_mailer.delivery_method = :smtp
+  host = 'example.com'
+  config.action_mailer.default_url_options = { host: host }
+  
+  # SMTP settings for gmail
+  config.action_mailer.smtp_settings = { 
+    :address                => "smtp.gmail.com",
+    :port                   => 587,
+    :user_name              => ENV['EMAIL'],
+    :password               => ENV['PASSWORD'],
+  }
+
   # Don't care if the mailer can't send.
-  config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.raise_delivery_errors = true
 
   # Make template changes take effect immediately.
   config.action_mailer.perform_caching = false
